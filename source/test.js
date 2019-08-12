fc8acb3d31c01cade39400f92c15659cda05e61d
dreamfighterlgw <124280170@qq.com>
2019-08-12 17:21:36 +0800

    '增加第一个测试用的JS‘

---
+++ source/test.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Text,View} from 'react-native';
+export default class Test extends Component{
+
+    render(){
+        return(
+            <View>
+                <Text>
+                    无限风光在险峰
+                </Text>
+            </View>
+        )
+    }
+} 
